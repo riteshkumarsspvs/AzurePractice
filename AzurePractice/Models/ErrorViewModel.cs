20452a2a0bb90c6864c602320f4a04e532854ba7
riteshkumarssvps <riteshkumarssvps@gmail.com>
2023-03-21 04:46:50 +0530

    Add project files.

---
+++ AzurePractice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzurePractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
